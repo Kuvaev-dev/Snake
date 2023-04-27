757edb50f2f5f9e5eb2b99b68ffff142b6413df6
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2023-04-27 22:42:01 +0300

    Winmm Added

--- Snake.cpp
+++ Snake.cpp
@@ -1,7 +1,12 @@
 ﻿#include <iostream>
-#include "raylib.h"
+#include <raylib.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    const int GAME_WINDOW_WIDTH = 750, GAME_WINDOW_HEIGHT = 750;
+    cout << "Start...";
+    InitWindow(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT, "Snake | Kuvaiev Mykyta");
+    system("pause");
+    return 0;
 }
